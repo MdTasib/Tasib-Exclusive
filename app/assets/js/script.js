ec79378d7fe3eeb5a5a7d54ee0dd58aaca19c6e0
ohidul alam <mdtasib442@gmali.com>
2021-01-04 18:02:49 +0600

    worning site

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,11 +1,11 @@
-var typed = new Typed(".typed", {
-    strings: [
-        "WEB DESIGNE",
-        "WORDPRESS DEVELOPER",
-        "WOOCOMMERCE SPECIALIST",
-        "WORDPRESS THEME CUSTOMIZER"
-    ],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-});
+// var typed = new Typed(".typed", {
+//     strings: [
+//         "WEB DESIGNE",
+//         "WORDPRESS DEVELOPER",
+//         "WOOCOMMERCE SPECIALIST",
+//         "WORDPRESS THEME CUSTOMIZER"
+//     ],
+//     typeSpeed: 150,
+//     backSpeed: 150,
+//     loop: true
+// });
