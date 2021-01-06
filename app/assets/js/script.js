d8014206557dc37a1a3103ce863a18b0baeaf1f5
ohidul alam <mdtasib442@gmali.com>
2021-01-06 11:40:42 +0600

    working

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,3 +1,4 @@
+// typed design function
 // var typed = new Typed(".typed", {
 //     strings: [
 //         "WEB DESIGNE",
@@ -9,3 +10,40 @@
 //     backSpeed: 150,
 //     loop: true
 // });
+// initialize counterup 
+if ($('span').hasClass('counter')) {
+    $('.counter').counterUp({
+        delay: 15,
+        time: 9000
+    })
+}
+
+
+// owl carousel slider
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        600: {
+            items: 3,
+            nav: true
+        },
+        1000: {
+            items: 3,
+            nav: true,
+            loop: true
+        }
+    }
+})
+
